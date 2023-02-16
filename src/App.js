b5ec3b631e5c896ba04b6d070cbacacef7ef3db7
Vijay <vijayktpr1@gmail.com>
2023-02-16 11:13:17 +0530

    Text change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React by Vijay Kumar
+          Learn React by Vijay Tiwari
         </a>
       </header>
     </div>
