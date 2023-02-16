228391c596473a42d8c8e56054751234759f6ff3
Vijay <vijayktpr1@gmail.com>
2023-02-16 11:06:01 +0530

    sample change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React by Vijay 
+          Learn React by Vijay Kumar
         </a>
       </header>
     </div>
