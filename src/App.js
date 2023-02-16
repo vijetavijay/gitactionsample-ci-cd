8a0c5f38fbb34e3311fba11b4a8ace9670a62d03
Vijay <vijayktpr1@gmail.com>
2023-02-16 12:58:16 +0530

    Updated files

--- src/App.js
+++ src/App.js
@@ -23,7 +23,14 @@ function App() {
 <div className="w3-row-padding w3-padding-64 w3-container">
   <div className="w3-content">
     <div className="w3-twothird">
-      <h1>Lorem Ipsum</h1>
+      <h1><a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React by XYZ
+        </a></h1>
       <h5 className="w3-padding-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
 
       <p className="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
@@ -32,7 +39,8 @@ function App() {
     </div>
 
     <div className="w3-third w3-center">
-      <i className="fa fa-anchor w3-padding-64 w3-text-red"></i>
+    <img src="https://cdn.northerntrust.com/common/images/nt-logo/black/nt-stack-2-line.png" class="ntLogo" alt="Northern Trust" />
+
     </div>
   </div>
 </div>
